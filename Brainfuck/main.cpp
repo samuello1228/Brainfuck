751847a70cfaf90c2517d65a8f8ebd079bd96e9b
Samuel Lo <locheukyee2001@gmail.com>
2017-12-05 20:46:56 +0800

    first commit for interpreter

--- Brainfuck/main.cpp
+++ Brainfuck/main.cpp
@@ -7,9 +7,284 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void Run(string code,string input,string output)
+{
+    //input source code
+    ifstream src(code.c_str());
+    vector<int> binary;
+    binary.reserve(5000);
+    int loopCheck = 0;
+    while(true)
+    {
+        char srcCellChar;
+        src.get(srcCellChar);
+        
+        if(src.eof())
+        {
+            break;
+        }
+        
+        int srcCellInt = int(srcCellChar);
+        if(srcCellInt==43 ||
+           srcCellInt==44 ||
+           srcCellInt==45 ||
+           srcCellInt==46 ||
+           srcCellInt==60 ||
+           srcCellInt==62 ||
+           srcCellInt==91 ||
+           srcCellInt==93 )
+        {
+            if(srcCellInt==91)
+            {
+                loopCheck++;
+            }
+            else if(srcCellInt==93 )
+            {
+                loopCheck--;
+            }
+            
+            if(loopCheck<0)
+            {
+                cout<<"Error: The binary file has unmatched [."<<endl;
+                return;
+            }
+            
+            binary.push_back(srcCellInt);
+        }
+        else
+        {
+            cout<<"Error: The binary file contains other character: "<<srcCellInt<<endl;
+            return;
+        }
+    }
+    
+    if(binary.size()==0)
+    {
+        cout<<"Error: The binary file is empty."<<endl;
+        return;
+    }
+    
+    if(loopCheck!=0)
+    {
+        cout<<"Error: The binary file has unmatched [ or ]."<<endl;
+        return;
+    }
+    src.close();
+    
+    //Run
+    ifstream fin(input.c_str());
+    ofstream fout(output.c_str());
+    int binPointer = 0;
+    bool inputIsEnd = false;
+    
+    vector<int> array;
+    array.reserve(5000);
+    array.push_back(0);
+    int cellPointer = 0;
+    while(true)
+    {
+        if(binary[binPointer]==43)
+        {
+            //+
+            if(array[cellPointer]==255)
+            {
+                cout<<"Error: The cell "<<cellPointer<<" is overflow."<<endl;
+                return;
+            }
+            else array[cellPointer]++;
+            
+            binPointer++;
+        }
+        else if(binary[binPointer]==44)
+        {
+            //,
+            if(inputIsEnd)
+            {
+                cout<<"Error: The input file is already ended."<<endl;
+                return;
+            }
+            
+            char inputChar;
+            fin.get(inputChar);
+            
+            int inputInt;
+            if(fin.eof())
+            {
+                inputIsEnd = true;
+                inputInt = 0;
+                cout<<"Info: The input file is ended."<<endl;
+            }
+            else
+            {
+                inputInt = int(inputChar);
+                if(inputInt<=9 || (inputInt>=11 && inputInt<=31) || inputInt>=127)
+                {
+                    cout<<"Error: The input character "<<inputInt<<" cannot be printable."<<endl;
+                    return;
+                }
+            }
+            
+            array[cellPointer]=inputInt;
+            
+            binPointer++;
+        }
+        else if(binary[binPointer]==45)
+        {
+            //-
+            if(array[cellPointer]==0)
+            {
+                cout<<"Error: The cell "<<cellPointer<<" is underflow."<<endl;
+                return;
+            }
+            else array[cellPointer]--;
+            
+            binPointer++;
+        }
+        else if(binary[binPointer]==46)
+        {
+            //.
+            if(array[cellPointer]<=31 || array[cellPointer]>=127)
+            {
+                cout<<"Error: The output character "<<array[cellPointer]<<" cannot be printable at cell "<<cellPointer<<endl;
+                return;
+            }
+            else
+            {
+                fout<<char(array[cellPointer]);
+            }
+            
+            binPointer++;
+        }
+        else if(binary[binPointer]==60)
+        {
+            ///<
+            if(cellPointer==0)
+            {
+                cout<<"Error: The cell pointer is already leftmost, cannnot further move left."<<endl;
+                return;
+            }
+            else cellPointer--;
+            
+            binPointer++;
+        }
+        else if(binary[binPointer]==62)
+        {
+            //>
+            if(cellPointer == array.size()-1)
+            {
+                array.push_back(0);
+            }
+            
+            cellPointer++;
+            
+            binPointer++;
+        }
+        else if(binary[binPointer]==91)
+        {
+            //[
+            if(array[cellPointer]==0)
+            {
+                int binPointerTemp = binPointer;
+                int count = 0;
+                while(true)
+                {
+                    binPointerTemp++;
+                    if(binPointerTemp == binary.size())
+                    {
+                        cout<<"Error: The corresponding ] cannot be found, for the [ at "<<binPointer<<endl;
+                        return;
+                    }
+                    
+                    if(binary[binPointerTemp]==91)
+                    {
+                        count++;
+                    }
+                    else if(binary[binPointerTemp]==93)
+                    {
+                        if(count == 0)
+                        {
+                            binPointer = binPointerTemp+1;
+                            break;
+                        }
+                        else
+                        {
+                            count--;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                binPointer++;
+            }
+        }
+        else if(binary[binPointer]==93)
+        {
+            //]
+            if(array[cellPointer]==0)
+            {
+                binPointer++;
+            }
+            else
+            {
+                int binPointerTemp = binPointer;
+                int count = 0;
+                while(true)
+                {
+                    binPointerTemp--;
+                    if(binPointerTemp == -1)
+                    {
+                        cout<<"Error: The corresponding [ cannot be found, for the ] at "<<binPointer<<endl;
+                        return;
+                    }
+                    
+                    if(binary[binPointerTemp]==91)
+                    {
+                        if(count == 0)
+                        {
+                            binPointer = binPointerTemp+1;
+                            break;
+                        }
+                        else
+                        {
+                            count--;
+                        }
+                    }
+                    else if(binary[binPointerTemp]==93)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        if(binPointer == binary.size())
+        {
+            cout<<"Info: The program is ended."<<endl;
+            break;
+        }
+    }
+    
+    fin.close();
+    fout.close();
+}
+
+int main()
+{
+    ofstream bin0("data/bin0.cpp");
+    bin0<<",[.,]";
+    bin0.close();
+    
+    ofstream src1("data/src1.cpp");
+    src1<<",[.,]";
+    src1.close();
+    
+    Run("data/bin0.cpp","data/src1.cpp","data/bin1.cpp");
+    //Run("data/bin1.cpp","data/src2.cpp","data/bin2.cpp");
     return 0;
 }
