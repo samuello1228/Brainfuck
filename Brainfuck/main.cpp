abdf188d4272b13bb62189a126a607e010afc802
Samuel Lo <locheukyee2001@gmail.com>
2017-12-05 15:15:50 +0800

    Initial Commit

---
+++ Brainfuck/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Brainfuck
+//
+//  Created by Samuel Lo on 5/12/2017.
+//  Copyright © 2017年 Samuel Lo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
