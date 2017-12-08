63c3c3babd1ce0b74eb3dc3fa42d11295bd086d3
Samuel Lo <locheukyee2001@gmail.com>
2017-12-09 00:44:09 +0800

    add quotation for generator_copy

--- Products/Debug/data/v2/src/generator_copy.cpp
+++ Products/Debug/data/v2/src/generator_copy.cpp
@@ -13,83 +13,83 @@
 //cell 6: constant value cell: bool for loop ;or output
 
 //initialize the value in the generator to 3
-+>>>>+++>>+++<<<<<<
+"+>>>>+++>>+++<<<<<<"
 
 //For the case that cell 4 is filled
 {
     //if cell 0 is true; copy cell 4 to cell 5 and cell x = 7
-    [
+    "["
     //set cell 0 to false; set aux cell 1 to true
-    ->+
+    "->+"
     //go to cell 4
-    >>>
+    ">>>"
     
     //copy cell 4 to cell 5 and cell x = 7
     {
         //copy cell 4 to cell 5
-        [->+
+        "[->+"
         //go to cell 6
-        >
+        ">"
         //go to cell x = 7; n = 1
-        >
+        ">"
         //copy cell 4 to cell 7
-        +
+        "+"
         //go to cell 6; n = 1
-        <
+        "<"
         //go to cell 4
-        <<
-        ]
+        "<<"
+        "]"
     }
     
     //go to cell 0
-    <<<<
-    ]
+    "<<<<"
+    "]"
 }
 
 //For the case that cell 5 is filled
 {
     //if cell 2 is true; copy cell 5 to cell 4 and cell x = 7
-    >>[
+    ">>["
     //set cell 2 to false; set aux cell 3 to true
-    ->+
+    "->+"
     //go to cell 5
-    >>
+    ">>"
     
     //copy cell 5 to cell 4 and cell x = 7
     {
         //copy cell 5 to cell 4
-        [-<+
+        "[-<+"
         //go to cell 6
-        >>
+        ">>"
         //go to cell x = 7; n = 1
-        >
+        ">"
         //copy cell 5 to cell 7
-        +
+        "+"
         //go to cell 6; n = 1
-        <
+        "<"
         //go to cell 5
-        <
-        ]
+        "<"
+        "]"
     }
     
     //go to cell 2
-    <<<
-    ]
+    "<<<"
+    "]"
 }
 
 {
     //go to aux cell 1
-    <
+    "<"
     //if cell 1 is true; set cell 1 to false; set cell 2 to true
-    [->+<]
+    "[->+<]"
     
     //go to aux cell 3
-    >>
+    ">>"
     //if cell 3 is true; set cell 3 to false; set cell 0 to true
-    [-<<<+>>>]
+    "[-<<<+>>>]"
     
     //go to cell 0
-    <<<
+    "<<<"
 }
 
 //final result:
