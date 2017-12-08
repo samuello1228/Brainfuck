f2f51a694bb7b44a4c69b13050f5a9920200c5fe
Samuel Lo <locheukyee2001@gmail.com>
2017-12-09 00:53:11 +0800

    add quotation for mapping_initialize

--- Products/Debug/data/v2/src/mapping_initialize.cpp
+++ Products/Debug/data/v2/src/mapping_initialize.cpp
@@ -3,7 +3,7 @@
 //require 6 cells (continuous)
 //cell 0: always 0 for synchronization
 //cell 1: always 1 for synchronization
->+<
+">+<"
 //cell 2: input (destructive)
 //cell 3: always 0 for stopping the loop
 //cell 4: bool
