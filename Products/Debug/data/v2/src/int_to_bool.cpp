b3f2c1270d256ec4c90f8864db05442c806c9400
Samuel Lo <locheukyee2001@gmail.com>
2017-12-09 00:49:32 +0800

    add quotation for int_to_bool

--- Products/Debug/data/v2/src/int_to_bool.cpp
+++ Products/Debug/data/v2/src/int_to_bool.cpp
@@ -3,34 +3,34 @@
 //require 4 cells (continuous)
 //cell 0: always 0; for synchronizing the pointer after loop
 //cell 1: input cell (not destructive)
->+++
+">+++"
 //cell 2: bool result
 //cell 3: set 0 for stopping the loop
 
 //go to cell 0
-<
+"<"
 
 //Run
 {
     //go to cell 1
-    >
+    ">"
     
     {
         //if cell 1 is not zero
-        [
+        "["
         //go to cell 2
-        >
+        ">"
         //set cell 2 to true
-        +
+        "+"
         //go to cell 3 to stop the loop
-        >]
+        ">]"
     }
     
     //if input is zero; go to cell 0
     //if input is not zero; avoid 0 at cell 3; go to cell 2 and its value must be 1
-    <
+    "<"
     //go to cell 0 by loop; synchronization
-    [<]
+    "[<]"
 }
 
 //final result:
