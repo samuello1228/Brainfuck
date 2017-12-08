3b8adb8586116623f18349e47582e270697bce14
Samuel Lo <locheukyee2001@gmail.com>
2017-12-09 00:56:22 +0800

    add quotation for NOT_gate

--- Products/Debug/data/v2/src/NOT_gate.cpp
+++ Products/Debug/data/v2/src/NOT_gate.cpp
@@ -1,19 +1,19 @@
 //NOT gate
 //require 2 cells (discrete)
 //cell 0 is input
-+
+"+"
 //go to cell 1
->
+">"
 
 {
     //set 1 at cell 1
-    +
+    "+"
     //go to cell 0
-    <
+    "<"
     //subtract cell 0 by cell 1
-    [->-<]
+    "[->-<]"
     //go to cell 1;
-    >
+    ">"
 }
 
 //final result:
