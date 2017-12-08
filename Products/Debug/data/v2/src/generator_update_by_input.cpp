6deed4ee8cc497f3f809b8586ad074069ca563e3
Samuel Lo <locheukyee2001@gmail.com>
2017-12-09 00:46:56 +0800

    add quotation for generator_update_by_input

--- Products/Debug/data/v2/src/generator_update_by_input.cpp
+++ Products/Debug/data/v2/src/generator_update_by_input.cpp
@@ -14,86 +14,86 @@
 //For the case that cell 4 is filled
 {
     //if cell 0 is true;
-    [
+    "["
     //set cell 0 to false; set aux cell 1 to true
-    ->+
+    "->+"
     //go to cell 4
-    >>>
+    ">>>"
     
     //update the value
     //update by input
     {
         //clear cell 4
-        [-]
+        "[-]"
         //go to cell 6
-        >>
+        ">>"
         //clear cell 6
-        [-]
+        "[-]"
         
         //go to cell 5
-        <
+        "<"
         //input
-        ,
+        ","
         //copy cell 5 to cell 4 and 6
-        [-<+>>+<]
+        "[-<+>>+<]"
         
         //go to cell 4
-        <
+        "<"
     }
     
     //go to cell 0
-    <<<<
-    ]
+    "<<<<"
+    "]"
 }
 
 //For the case that cell 5 is filled
 {
     //if cell 2 is true;
-    >>[
+    ">>["
     //set cell 2 to false; set aux cell 3 to true
-    ->+
+    "->+"
     //go to cell 5
-    >>
+    ">>"
     
     //update the value
     //update by input
     {
         //clear cell 5
-        [-]
+        "[-]"
         //go to cell 6
-        >
+        ">"
         //clear cell 6
-        [-]
+        "[-]"
         
         //go to cell 4
-        <<
+        "<<"
         //input
-        ,
+        ","
         //copy cell 4 to cell 5 and 6
-        [->+>+<<]
+        "[->+>+<<]"
         
         //go to cell 5
-        >
+        ">"
     }
     
     //go to cell 2
-    <<<
-    ]
+    "<<<"
+    "]"
 }
 
 {
     //go to aux cell 1
-    <
+    "<"
     //if cell 1 is true; set cell 1 to false; set cell 0 to true
-    [-<+>]
+    "[-<+>]"
     
     //go to aux cell 3
-    >>
+    ">>"
     //if cell 3 is true; set cell 3 to false; set cell 2 to true
-    [-<+>]
+    "[-<+>]"
     
     //go to cell 0
-    <<<
+    "<<<"
 }
 
 //final result:
