bb140d63f399dfb291ea7384895728c1cfbdb0f0
Samuel Lo <locheukyee2001@gmail.com>
2017-12-09 21:12:54 +0800

    better documentation for compiler in v3

--- Products/Debug/data/v2/src/next_compiler.cpp
+++ Products/Debug/data/v2/src/next_compiler.cpp
@@ -1,5 +1,5 @@
-//remove line comment
-//remove any character between / and newline
+//remove line comment (remove any character between / and newline)
+//remove all characters except the 8 special characters (if it is not line comment)
 
 //include is_same
 //include mapping_initialize
