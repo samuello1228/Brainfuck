b26f36d4c109deabb35bb5508715cec28e123336
Samuel Lo <locheukyee2001@gmail.com>
2018-08-10 05:05:44 +0800

    change the order of bool cell

--- Products/Debug/data/v4/src/next_compiler2.cpp
+++ Products/Debug/data/v4/src/next_compiler2.cpp
@@ -19,24 +19,23 @@
 //cell 51: input cell (zero after copying)
 ">>"
 
-//cell 52: bool for the case that the input character is line comment
+//cell 52: bool for the case that the input character is not line comment and outside quote
 //cell 53: aux cell for cell 52 (continues; not redo any 52 bool if block)
 //cell 54: aux cell for cell 52 (do other 52 bool if block)
-">>>"
+">+"
 
-//cell 55: bool for the case that the input character is not line comment and outside quote
+//cell 55: bool for the case that the input character is not line comment and inside quote
 //cell 56: aux cell for cell 55 (continues; not redo any 55 bool if block)
 //cell 57: aux cell for cell 55 (do other 55 bool if block)
-">+>>"
 
-//cell 58: bool for the case that the input character is not line comment and inside quote
+//cell 58: bool for the case that the input character is line comment
 //cell 59: aux cell for cell 58 (continues; not redo any 58 bool if block)
 //cell 60: aux cell for cell 58 (do other 58 bool if block)
 
 //fill the value of input
 {
     //go to cell 51
-    "<<<<<<"
+    "<"
     //get input
     ","
 }
@@ -62,16 +61,16 @@
     
     //For the case that the input character is not line comment and outside quote
     {
-        //go to cell 55 (add 4)
-        ">>>>"
-        //if cell 55 is true
+        //go to cell 52
+        ">"
+        //if cell 52 is true
         "[-"
         
         //check whether the input is /
         {
             {
                 //go to input (cell 49)
-                "<<<<<<"
+                "<<<"
                 
                 //copy to cell 23
                 //k = 23 - 49 = -26
@@ -135,19 +134,21 @@
                 ">>"
                 "[-"
                 
-                //go to aux bool cell 53 (add 29)
+                //go to aux bool cell 59 (add 35)
                 ">>>>>>>>>>"
                 ">>>>>>>>>>"
-                ">>>>>>>>>"
+                ">>>>>>>>>>"
+                ">>>>>"
                 
-                //set aux bool cell 53 to true
+                //set aux bool cell 59 to true
                 "+"
                 //change to the case that the input character is line comment
                 
-                //go to cell 24 (minus 29)
+                //go to cell 24 (minus 35)
                 "<<<<<<<<<<"
                 "<<<<<<<<<<"
-                "<<<<<<<<<"
+                "<<<<<<<<<<"
+                "<<<<<"
                 "]"
             }
             
@@ -157,65 +158,64 @@
                 "<<"
                 "[-"
                 
-                //go to aux bool cell 57 (add 35)
+                //go to aux bool cell 54 (add 32)
                 ">>>>>>>>>>"
                 ">>>>>>>>>>"
                 ">>>>>>>>>>"
-                ">>>>>"
+                ">>"
                 
-                //set aux bool cell 57 to true
+                //set aux bool cell 54 to true
                 "+"
                 //For running the following case that the input character is not line comment
                 //Further check whether the input character is quote
                 
-                //go to cell 22 (minus 35)
+                //go to cell 22 (minus 32)
                 "<<<<<<<<<<"
                 "<<<<<<<<<<"
                 "<<<<<<<<<<"
-                "<<<<<"
+                "<<"
                 "]"
             }
         }
         
-        //go to cell 55 (add 33)
+        //go to cell 52 (add 30)
         ">>>>>>>>>>"
         ">>>>>>>>>>"
         ">>>>>>>>>>"
-        ">>>"
         "]"
     }
     
-    //For further checking whether the input character is quote, if cell 57 is true
+    //For further checking whether the input character is quote, if cell 54 is true
     {
-        //go to aux bool cell 57
+        //go to aux bool cell 54
         ">>"
-        //if cell 57 is true
+        //if cell 54 is true
         "["
-        //set aux bool cell 57 to false
+        //set aux bool cell 54 to false
         "-"
         
-        //go to aux bool cell 55
+        //go to aux bool cell 52
         "<<"
-        //set aux bool cell 55 to true
+        //set aux bool cell 52 to true
         "+"
         
-        //go to aux bool cell 57
+        //go to aux bool cell 54
         ">>"
         "]"
     }
     
     //For further checking whether the input character is quote
     {
-        //go to cell 55
+        //go to cell 52
         "<<"
-        //if cell 55 is true
+        //if cell 52 is true
         "[-"
         
         //check whether the input is quote
         {
             {
                 //go to input (cell 49)
-                "<<<<<<"
+                "<<<"
                 
                 //copy to cell 23
                 //k = 23 - 49 = -26
@@ -279,21 +279,21 @@
                 ">>"
                 "[-"
                 
-                //go to aux bool cell 59 (add 35)
+                //go to aux bool cell 56 (add 32)
                 ">>>>>>>>>>"
                 ">>>>>>>>>>"
                 ">>>>>>>>>>"
-                ">>>>>"
+                ">>"
                 
-                //set aux bool cell 59 to true
+                //set aux bool cell 56 to true
                 "+"
                 //change to the case that the input character is not line comment and inside quote
                 
-                //go to cell 24 (minus 35)
+                //go to cell 24 (minus 32)
                 "<<<<<<<<<<"
                 "<<<<<<<<<<"
                 "<<<<<<<<<<"
-                "<<<<<"
+                "<<"
                 "]"
             }
             
@@ -303,45 +303,44 @@
                 "<<"
                 "[-"
                 
-                //go to aux bool cell 56 (add 34)
+                //go to aux bool cell 53 (add 31)
                 ">>>>>>>>>>"
                 ">>>>>>>>>>"
                 ">>>>>>>>>>"
-                ">>>>"
+                ">"
                 
-                //set aux bool cell 56 to true
+                //set aux bool cell 53 to true
                 "+"
                 //change to the case that the input character is not line comment and outside quote
                 
-                //go to cell 22 (minus 34)
+                //go to cell 22 (minus 31)
                 "<<<<<<<<<<"
                 "<<<<<<<<<<"
                 "<<<<<<<<<<"
-                "<<<<"
+                "<"
                 "]"
             }
         }
         
-        //go to cell 55 (add 33)
+        //go to cell 52 (add 30)
         ">>>>>>>>>>"
         ">>>>>>>>>>"
         ">>>>>>>>>>"
-        ">>>"
         "]"
     }
     
     //For the case that the input character is line comment
     {
-        //go to cell 52
-        "<<<"
-        //if cell 52 is true
+        //go to cell 58
+        ">>>>>>"
+        //if cell 58 is true
         "[-"
         
         //check whether the input is newline
         {
             {
                 //go to input (cell 49)
-                "<<<"
+                "<<<<<<<<<"
                 
                 //copy to cell 23
                 //k = 23 - 49 = -26
@@ -403,21 +402,19 @@
                 ">>"
                 "[-"
                 
-                //go to aux bool cell 56 (add 32)
+                //go to aux bool cell 53 (add 29)
                 ">>>>>>>>>>"
                 ">>>>>>>>>>"
-                ">>>>>>>>>>"
-                ">>"
+                ">>>>>>>>>"
                 
-                //set aux bool cell 56 to true
+                //set aux bool cell 53 to true
                 "+"
                 //change to the case that the input character is not line comment and outside quote
                 
-                //go to cell 24 (minus 32)
+                //go to cell 24 (minus 29)
                 "<<<<<<<<<<"
                 "<<<<<<<<<<"
-                "<<<<<<<<<<"
-                "<<"
+                "<<<<<<<<<"
                 "]"
             }
             
@@ -427,44 +424,45 @@
                 "<<"
                 "[-"
                 
-                //go to aux bool cell 53 (add 31)
+                //go to aux bool cell 59 (add 37)
                 ">>>>>>>>>>"
                 ">>>>>>>>>>"
                 ">>>>>>>>>>"
-                ">"
+                ">>>>>>>"
                 
-                //set aux bool cell 53 to true
+                //set aux bool cell 59 to true
                 "+"
                 //change to the case that the input character is line comment
                 
-                //go to cell 22 (minus 31)
+                //go to cell 22 (minus 37)
                 "<<<<<<<<<<"
                 "<<<<<<<<<<"
                 "<<<<<<<<<<"
-                "<"
+                "<<<<<<<"
                 "]"
             }
         }
         
-        //go to cell 52 (add 30)
+        //go to cell 58 (add 36)
         ">>>>>>>>>>"
         ">>>>>>>>>>"
         ">>>>>>>>>>"
+        ">>>>>>"
         "]"
     }
     
     //For the case that the input character is not line comment and inside quote
     {
-        //go to cell 58
-        ">>>>>>"
-        //if cell 58 is true
+        //go to cell 55
+        "<<<"
+        //if cell 55 is true
         "[-"
         
         //check whether the input is quote
         {
             {
-                //go to input (cell 49) (minus 9)
-                "<<<<<<<<<"
+                //go to input (cell 49)
+                "<<<<<<"
                 
                 //copy to cell 23
                 //k = 23 - 49 = -26
@@ -529,21 +527,19 @@
                 ">>"
                 "[-"
                 
-                //go to aux bool cell 56 (add 32)
+                //go to aux bool cell 53 (add 29)
                 ">>>>>>>>>>"
                 ">>>>>>>>>>"
-                ">>>>>>>>>>"
-                ">>"
+                ">>>>>>>>>"
                 
-                //set aux bool cell 56 to true
+                //set aux bool cell 53 to true
                 "+"
                 //change to the case that the input character is not line comment and outside quote
                 
-                //go to cell 24 (minus 32)
+                //go to cell 24 (minus 29)
                 "<<<<<<<<<<"
                 "<<<<<<<<<<"
-                "<<<<<<<<<<"
-                "<<"
+                "<<<<<<<<<"
                 "]"
             }
             
@@ -560,44 +556,44 @@
                 //output
                 "."
                 
-                //go to aux bool cell 59 (add 9)
-                ">>>>>>>>>"
-                //set aux bool cell 59 to true
+                //go to aux bool cell 56
+                ">>>>>>"
+                //set aux bool cell 56 to true
                 "+"
-                //change to the case that the input character is not line comment and outside quote
+                //change to the case that the input character is not line comment and inside quote
                 
-                //go to cell 22 (minus 37)
+                //go to cell 22 (minus 34)
                 "<<<<<<<<<<"
                 "<<<<<<<<<<"
                 "<<<<<<<<<<"
-                "<<<<<<<"
+                "<<<<"
                 "]"
             }
         }
         
-        //go to cell 58 (add 36)
+        //go to cell 55 (add 33)
         ">>>>>>>>>>"
         ">>>>>>>>>>"
         ">>>>>>>>>>"
-        ">>>>>>"
+        ">>>"
         "]"
     }
     
     //reset all bool cells
     {
         //go to aux cell 53
-        "<<<<<"
-        //if cell 111 is true; set cell 53 to false; set cell 52 to true
+        "<<"
+        //if cell 53 is true; set cell 53 to false; set cell 52 to true
         "[-<+>]"
         
         //go to aux cell 56
         ">>>"
-        //if cell 114 is true; set cell 56 to false; set cell 55 to true
+        //if cell 56 is true; set cell 56 to false; set cell 55 to true
         "[-<+>]"
         
         //go to aux cell 59
         ">>>"
-        //if cell 114 is true; set cell 59 to false; set cell 58 to true
+        //if cell 59 is true; set cell 59 to false; set cell 58 to true
         "[-<+>]"
     }
     
